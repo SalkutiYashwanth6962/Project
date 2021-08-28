0ba3eb9d6c92cacf37ce06b96df604921b4b7794
Vasumithra Therli <vasumithra@192.168.0.103>
2021-08-28 18:16:04 +0530

    Fixes for session changes...

--- src/main/java/com/picktime/website/Constants.java
+++ src/main/java/com/picktime/website/Constants.java
@@ -4,7 +4,7 @@ import com.google.appengine.api.utils.SystemProperty;
 
 public class Constants 
 {
-	public static final String App_version								=	"v21519v2";
+	public static final String App_version								=	"v21818v3";
 	public static final String APP_NAME									=	"Picktime";
 	protected static String appID										= 	System.getProperty(SystemProperty.applicationId.key());
 }
