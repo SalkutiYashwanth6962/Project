20bf117bbfa9ebadbc2744d56e082fd0ca47cd76
Hrishabh Kumar <hrishabhkumar@users.noreply.github.com>
2024-02-18 22:51:23 +0530

    java 11 upgrade

---
+++ watch_resources/index.js
@@ -0,0 +1,12 @@
+import chokidar from "chokidar";
+import path from 'path';
+import child_process from "child_process";
+
+const filePath = path.resolve('../src/main/webapp');
+var watcher = chokidar.watch(filePath, {ignored: /(^|[\/\\])\../, persistent: true});
+const mainDir = path.resolve('../');
+watcher
+  .on('all', function(event, path) { console.log('File', path, 'has been ', event); })
+  .on('change', function(event, path) { console.log('File', path, 'has been ', event); child_process.execSync('mvn war:exploded', {
+    cwd: mainDir
+  }, console.log)})
